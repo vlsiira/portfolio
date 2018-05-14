1ab9ba9815ff410195c3f6967c809caa56bb042e
Valerie Siira <vlsiira@vt.edu>
2018-05-13 17:30:12 -0700

    clicking icon menu makes ul appear, clicking cross makes them disappear

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,16 @@ Project.prototype.toHtml = function() {
     var $newArticle = $('article.template').clone();
 }
 
-$('#menu').on('click', function() {
-    $(this).toggleClass('icon-cross')
+$('#menu').on('click', function(event) {
+    event.preventDefault();
+    $(this).toggleClass('icon-cross');
+    if ($(this).hasClass ('icon-cross')) {
+        $('ul').show();
+    } else {
+        $('ul').hide();
+    }
 })
+
+// hideNav = function() {
+//     $('#menu').on
+// }
