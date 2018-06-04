7051b001645d6d926aeb82800fa8fd03c9ec1a91
Valerie Siira <vlsiira@vt.edu>
2018-06-04 16:12:38 -0700

    updated view, added li tag to projects section

--- public/scripts/views/projectView.js
+++ public/scripts/views/projectView.js
@@ -5,17 +5,22 @@ const app = app || {}; // 12
 (function(module) {
     const projectView = {
         init: function () {
-            
+            $('main').removeAttr('class').addClass('repos');
+
+            app.repos.all.forEach(repo => {
+                $('#projects ul').append(`<li>${repo.name} IS ONLINE AT ${repo.url}</li>`);
+            })
         }
     };
 
-    projectView.initIndexPage = () => {
-        Project.all.forEach(project => {
-            $('#projects').append(project.toHtml('#project-template'));
-        })
-    }
+    // projectView.initIndexPage = () => {
+    //     Project.all.forEach(project => {
+    //         $('#projects').append(project.toHtml('#project-template'));
+    //     })
+    // }
 
-    Project.fetchAll(projectView.initIndexPage);
+    // Project.fetchAll(projectView.initIndexPage);
     module.projectView = projectView;
 
-})(window);
+})(app);
+// })(window);
